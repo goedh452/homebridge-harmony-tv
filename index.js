291730bbd9f7bb14b5a6b6d5e7fb015808db2d7c
goedh452 <bg.goedhart@gmail.com>
2019-09-07 17:21:18 +0200

    Update index.js

--- index.js
+++ index.js
@@ -10,9 +10,9 @@ module.exports = function(homebridge) {
 function HttpSprinkler(log, config) {
   this.log = log;
 	
-	this.name = config.name || "Harmony TV";
+  this.name = config.name || "Harmony TV";
 	
-	this.manufacturer = config.manufacturer || "goedh452";
+  this.manufacturer = config.manufacturer || "goedh452";
   this.model = config.model || "Harmony TV";
   this.serial = config.serial || "Harmony TV";
 }
@@ -20,43 +20,26 @@ function HttpSprinkler(log, config) {
 
 HarmonyTV.prototype {
 	
-	getServices: function() {
+  getServices: function()
+  {
 		
-		this.informationService = new Service.AccessoryInformation();
+    this.informationService = new Service.AccessoryInformation();
     this.informationService
       .setCharacteristic(Characteristic.Manufacturer, this.manufacturer)
       .setCharacteristic(Characteristic.Model, this.model)
       .setCharacteristic(Characteristic.SerialNumber, this.serial);
 		
-		this.tvService = new Service.Television(this.name, 'tvService');
+    this.tvService = new Service.Television(this.name, 'tvService');
+	
+    this.tvService
+      .setCharacteristic(Characteristic.ConfiguredName, this.name)
+      .setCharacteristic(Characteristic.SleepDiscoveryMode, Characteristic.SleepDiscoveryMode.ALWAYS_DISCOVERABLE);
 		
-		this.tvService
-			.setCharacteristic(Characteristic.ConfiguredName, this.name)
-    	.setCharacteristic(Characteristic.SleepDiscoveryMode, Characteristic.SleepDiscoveryMode.ALWAYS_DISCOVERABLE);
-		
-		this.tvService
-        .getCharacteristic(Characteristic.Active)
-        .on('get', this.getPowerState.bind(this))
-        .on('set', this.setPowerState.bind(this));
+    this.tvService
+      .getCharacteristic(Characteristic.Active)
+      .on('get', this.getPowerState.bind(this))
+      .on('set', this.setPowerState.bind(this));
 
-		this.tvService
-        .getCharacteristic(Characteristic.ActiveIdentifier)
-        .on('set', (inputIdentifier, callback) => {
-            this.log.debug('Input source changed, new input source identifier: %d, source appId: %s', inputIdentifier, this.inputAppIds[inputIdentifier]);
-            this.setAppSwitchState(true, callback, this.inputAppIds[inputIdentifier]);
-        });
-		
-    this.tvService
-        .getCharacteristic(Characteristic.RemoteKey)
-        .on('set', this.remoteKeyPress.bind(this));
-		
-    this.tvService
-        .getCharacteristic(Characteristic.PowerModeSelection)
-        .on('set', (newValue, callback) => {
-            this.log.debug('Requested tv settings (PowerModeSelection): ' + newValue);
-            this.setRemoteControlButtonState(true, callback, 'MENU');
-        });
-		
-		return [this.tvService, this.informationService];
-	}
+    return [this.tvService, this.informationService];
+  }
 };
