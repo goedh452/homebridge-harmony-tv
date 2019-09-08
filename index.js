3740dd92e286833a90eec53f04b6a37c076978d8
Bouke Goedhart <bouke@MacBook-Bouke-6.local>
2019-09-08 17:48:54 +0200

    Building initial version

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var Service, Characteristic;
+var request = require("request");
 
 module.exports = function(homebridge) {
   Service = homebridge.hap.Service;
@@ -7,44 +8,91 @@ module.exports = function(homebridge) {
 };
 
 
-function HarmonyTV(log, config) {
+function HarmonyTV(log, config)
+{
   this.log = log;
-	
-  this.name = config.name || "Harmony TV";
-	
+
+  this.name = config.name                 || "Harmony TV";
+
+  // Harmony API settings
+  this.ApiIP = config.ApiIP               || "192.168.1.117";
+  this.ApiPort = config.ApiPort           || 8282;
+
+  // Manufacturer information
   this.manufacturer = config.manufacturer || "goedh452";
-  this.model = config.model || "Harmony TV";
-  this.serial = config.serial || "Harmony TV";
+  this.model = config.model               || "Harmony TV";
+  this.serial = config.serial             || "Harmony TV";
+
+
+  // Get Harmony Hubs
+  var baseURL = this.ApiIP + ":" + this.ApiPort + "/hubs";
+
+  this.httpRequest(baseURL, "", "GET", function(error, response, responseBody)
+  {
+    if (error)
+    {
+      this.log('HTTP get hubs function failed: %s', error.message);
+      callback(error);
+    }
+    else
+    {
+      var jsonHub = JSON.parse(responseBody);
+      var hub  = jsonHub[0].hubs;
+
+      this.log("JSON responsebody: " + jsonHub);
+      this.log("HUB received: " + hub);
+    }
+  }.bind(this));
+
+  // Get hub activities
+  var activitiesURL = baseURL + "/" + hub + "/activities";
+
+  this.httpRequest(activitiesURL, "", "GET", function(error, response, responseBody)
+  {
+    if (error)
+    {
+      this.log('HTTP get activities function failed: %s', error.message);
+      callback(error);
+    }
+    else
+    {
+      var jsonAct     = JSON.parse(responseBody);
+      var activities  = jsonAct[0].slug;
+
+      this.log("JSON responsebody: " + jsonAct);
+      this.log("Activities received: " + activities);
+    }
+  }.bind(this));
 }
 
 
 HarmonyTV.prototype = {
-	
+
   getPowerState: function(callback)
   {
 
   },
-	
+
   setPowerState: function(callback)
   {
 
   },
-	
+
   getServices: function()
   {
-		
+
     this.informationService = new Service.AccessoryInformation();
     this.informationService
       .setCharacteristic(Characteristic.Manufacturer, this.manufacturer)
       .setCharacteristic(Characteristic.Model, this.model)
       .setCharacteristic(Characteristic.SerialNumber, this.serial);
-		
+
     this.tvService = new Service.Television(this.name);
-	
+
     this.tvService
       .setCharacteristic(Characteristic.ConfiguredName, this.name)
       .setCharacteristic(Characteristic.SleepDiscoveryMode, Characteristic.SleepDiscoveryMode.ALWAYS_DISCOVERABLE);
-		
+
     this.tvService
       .getCharacteristic(Characteristic.Active)
       .on('get', this.getPowerState.bind(this))
